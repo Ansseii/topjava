9a8982e77c0f492b0331d7a39eaa6b150d28ffd4
Ansseii <a.lukash.92@gmail.com>
2018-09-21 15:49:17 +0300

    HW0 Optional 1 with streams

--- src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
@@ -21,7 +21,9 @@ public class UserMealsUtil {
                 new UserMeal(LocalDateTime.of(2015, Month.MAY, 31, 13, 0), "Обед", 500),
                 new UserMeal(LocalDateTime.of(2015, Month.MAY, 31, 20, 0), "Ужин", 510)
         );
-        getFilteredWithExceeded(mealList, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000);
+//        getFilteredWithExceeded(mealList, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000);
+        System.out.println(getFilteredWithExceeded(mealList, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000));
+        System.out.println(getFilteredWithExceededStream(mealList, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000));
 //        .toLocalDate();
 //        .toLocalTime();
     }
@@ -46,5 +48,16 @@ public class UserMealsUtil {
             calories.merge(meal.getDateTime().toLocalDate(), meal.getCalories(), Integer::sum);
         }
         return caloriesPerDay < calories.get(key.toLocalDate());
+    }
+
+    public static List<UserMealWithExceed> getFilteredWithExceededStream(final List<UserMeal> mealList, final LocalTime startTime,
+                                                                         final LocalTime endTime, final int caloriesPerDay) {
+        Map<LocalDate, Integer> calories = mealList.stream()
+                .collect(Collectors.groupingBy((date) -> date.getDateTime().toLocalDate(), Collectors.summingInt(UserMeal::getCalories)));
+        return mealList.stream()
+                .filter(u -> TimeUtil.isBetween(u.getDateTime().toLocalTime(), startTime, endTime))
+                .map(user -> new UserMealWithExceed(user.getDateTime(), user.getDescription(),
+                        user.getCalories(), calories.get(user.getDateTime().toLocalDate()) > caloriesPerDay))
+                .collect(Collectors.toList());
     }
 }
