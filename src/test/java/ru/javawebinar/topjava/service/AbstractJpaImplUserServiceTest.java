31432df4be5c75213116f2ec1488e91e1b8a7b1e
Ansseii <a.lukash.92@gmail.com>
2018-11-13 13:23:32 +0300

    HW6
    fixed all tests
    mvn test - all passed

---
+++ src/test/java/ru/javawebinar/topjava/service/AbstractJpaImplUserServiceTest.java
@@ -0,0 +1,19 @@
+package ru.javawebinar.topjava.service;
+
+import org.junit.Before;
+import org.springframework.beans.factory.annotation.Autowired;
+import ru.javawebinar.topjava.repository.JpaUtil;
+import ru.javawebinar.topjava.service.AbstractUserServiceTest;
+
+public abstract class AbstractJpaImplUserServiceTest extends AbstractUserServiceTest {
+
+    @Autowired
+    protected JpaUtil jpaUtil;
+
+    @Before
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+        jpaUtil.clear2ndLevelHibernateCache();
+    }
+}
