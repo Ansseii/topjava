193872d90b61093d254a224aff01aac8a6c6bde3
Ansseii <a.lukash.92@gmail.com>
2018-12-17 15:38:03 +0300

    HW 10 optional
    Added duplicate Email and meal Date\Time handlers.

--- src/main/java/ru/javawebinar/topjava/web/ExceptionInfoHandler.java
+++ src/main/java/ru/javawebinar/topjava/web/ExceptionInfoHandler.java
@@ -2,6 +2,9 @@ package ru.javawebinar.topjava.web;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.dao.DataIntegrityViolationException;
@@ -23,12 +26,22 @@ import ru.javawebinar.topjava.util.exception.NotFoundException;
 
 import javax.servlet.http.HttpServletRequest;
 
+import static ru.javawebinar.topjava.util.ValidationUtil.getRootCause;
 import static ru.javawebinar.topjava.util.exception.ErrorType.*;
 
 @RestControllerAdvice(annotations = RestController.class)
 @Order(Ordered.HIGHEST_PRECEDENCE + 5)
 public class ExceptionInfoHandler {
     private static Logger log = LoggerFactory.getLogger(ExceptionInfoHandler.class);
+    private static final String DUPLICATE_EMAIL_ERROR = "common.duplicateEmail";
+    private static final String DUPLICATE_DATE_TIME_ERROR = "common.duplicateMealDate";
+
+    private final MessageSource messageSource;
+
+    @Autowired
+    public ExceptionInfoHandler(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
 
     //  http://stackoverflow.com/a/22358422/548473
     @ResponseStatus(value = HttpStatus.UNPROCESSABLE_ENTITY)
@@ -40,6 +53,14 @@ public class ExceptionInfoHandler {
     @ResponseStatus(value = HttpStatus.CONFLICT)  // 409
     @ExceptionHandler(DataIntegrityViolationException.class)
     public ErrorInfo conflict(HttpServletRequest req, DataIntegrityViolationException e) {
+        String message;
+        message = getRootCause(e).getLocalizedMessage();
+        if (message.contains("users_unique_email_idx")) {
+            return new ErrorInfo(req.getRequestURL(), DATA_ERROR, messageSource.getMessage(DUPLICATE_EMAIL_ERROR, null, LocaleContextHolder.getLocale()));
+        }
+        if (message.contains("meals_unique_user_datetime_idx")) {
+            return new ErrorInfo(req.getRequestURL(), DATA_ERROR, messageSource.getMessage(DUPLICATE_DATE_TIME_ERROR, null, LocaleContextHolder.getLocale()));
+        }
         return logAndGetErrorInfo(req, e, true, DATA_ERROR);
     }
 
@@ -64,7 +85,7 @@ public class ExceptionInfoHandler {
 
     //    https://stackoverflow.com/questions/538870/should-private-helper-methods-be-static-if-they-can-be-static
     private static ErrorInfo logAndGetErrorInfo(HttpServletRequest req, Exception e, boolean logException, ErrorType errorType) {
-        Throwable rootCause = ValidationUtil.getRootCause(e);
+        Throwable rootCause = getRootCause(e);
         if (logException) {
             log.error(errorType + " at request " + req.getRequestURL(), rootCause);
         } else {
